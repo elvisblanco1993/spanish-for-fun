dd38ec9c79884f264f2d8918ff5a19b7470e507f
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2024-06-11 22:37:27 -0400

    Hide payment link from admins.

--- resources/views/livewire/request/index.blade.php
+++ resources/views/livewire/request/index.blade.php
@@ -42,7 +42,7 @@
                                 </td>
                                 <td class="px-3 py-2">
                                     <div class="flex items-center justify-end space-x-3">
-                                        @if (! $request->paid_at && $request->amount_due >= 100)
+                                        @if (Auth::user()->is_client && ! $request->paid_at && $request->amount_due >= 100)
                                             <a href="{{ route('checkout.service', ['request' => $request]) }}" class="whitespace-nowrap">{{__("Pay and submit")}}</a>
                                         @endif
                                         <a wire:navigate href="{{ route('request.show', ['request' => $request]) }}">Details</a>
